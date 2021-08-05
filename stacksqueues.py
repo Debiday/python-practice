ebf2b06b5e43ab64f088281b2088dd3f1ac3346d
Deborah Ong <deborah.oyt@gmail.com>
2021-08-04 23:13:05 -0700

    add stack notes

--- stacksqueues.py
+++ stacksqueues.py
@@ -81,5 +81,105 @@ class Deque(object):
     def size(self):
         return len(self.items)
 #_________________________________________________________
-#
+# Stack practice
 #_________________________________________________________
+class Stack2(object):
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+#_________________________________________________________
+# Queue practice
+#_________________________________________________________
+class Queue2(object):
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+#_________________________________________________________
+# Deque practice
+#_________________________________________________________
+class Deque2(object):
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0, item)
+
+    def removeFront(self):
+        return self.items.pop(0)
+
+    def removeRear(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+#_________________________________________________________
+# Balanced parenthesis check
+#_________________________________________________________
+def balance_check(s):
+
+    if len(s) % 2 != 0:
+        return False
+
+    opening = set('({[')
+    matches = set([('(',')'),('[',']'),('{', '}')])  
+
+    stack = []
+
+    for paren in s:
+        if paren in opening:
+            stack.append(paren)
+        else:
+            if len(stack) == 0:
+                return False
+                #no matching paren
+
+            last_open = stack.pop()
+
+            if (last_open, paren) not in matches:
+                return False
+            
+    return len(stack) == 0
+#_________________________________________________________
+# Queue two stacks
+#_________________________________________________________
+
+
+
+
+
+
+

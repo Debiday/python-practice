08498fbb15250255e5cc9ae7d35615ee8465c27f
Deborah Ong <deborah.oyt@gmail.com>
2021-08-05 21:04:18 -0700

    add notes

--- lists.py
+++ lists.py
@@ -23,14 +23,49 @@ class Node(object):
 # >>> b.nextnode = c
 # >>> a.nextnode.value //2
 #_________________________________________________________
-# 
+# Doubly linked list
 #_________________________________________________________
+# Allow greater variety of O(1) constant time update incl. insert/ delete
+# Header, Trailer, Sentinals (dummy nodes)
+
+class DoublyLinkedListNode(object):
+
+    def __init__(self, value):
+
+        self.value = value
+        self.nextnode = None
+        self.prevnode = None
+
+# >>> a = DoublyLinkedListNode(1)
+# >>> b = DoublyLinkedListNode(2)
+# >>> c = DoublyLinkedListNode(3)
+# >>> a.nextnode = b
+# >>> b.prevnode = a
+# >>> b.nextnode = c
+# >>> c.prevnode = b
 #_________________________________________________________
-# 
+# Singly linked list cycle check (Common!)
 #_________________________________________________________
+def cycle_check(node):
+
+    marker1 = node
+    marker2 = node
+
+    while marker2 != None and marker2.next_node != None:
+
+        marker1 = marker1.nextnode
+        marker2 = marker2.nextnode.nextnode
+
+        if marker2 == marker1:
+            return True
+        
+    return False
 #_________________________________________________________
-# 
+# Linked list reversal
 #_________________________________________________________
+
+
+
 #_________________________________________________________
 # 
 #_________________________________________________________

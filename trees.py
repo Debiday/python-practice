dc88019d9be834c4b8bc355ef426f802eb8ae632
Deborah Ong <deborah.oyt@gmail.com>
2021-08-15 12:21:26 -0700

    add bst notes

--- trees.py
+++ trees.py
@@ -384,7 +384,58 @@ class BinarySearchTree:
         return succ
 
 
-    def find
+    def findMin(self):
+
+        current = self
+        while current.hasLeftChild():
+            current = current.LeftChild
+        return current
+
+    def remove(self, currentNode):
+
+        if currentNode.isLeaf(): #leaf
+            if currentNode == currentNode.parent.leftChild:
+                currentNode.parent.leftChild = None
+            else: 
+                currentNode.parent.rightChild = None
+        elif currentNode.hasBothChildren(): #interior
+
+            succ = currentNode.findSuccessor()
+            succ.spliceOut()
+            currentNode.key = succ.key
+            currentNode.payload = succ.payload
+
+        else: #this node has 1 child
+            if currentNode.hasLeftChild():
+                if currentNode.isLeftChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.leftChild
+                elif currentNode.isRightChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.leftChild
+                    #note that it moves to the left!
+                else:
+
+                    currentNode.replaceNodeData(currentNode.leftChild.key,
+                                        currentNode.leftChild.payload,
+                                        currentNode.leftChild.leftChild,
+                                        currentNode.leftChild.rightChild)
+
+            else:
+
+                if currentNode.isLeftChild(): 
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.rightChild
+                elif currentNode.isRightChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.rightChild
+                else:
+                    currentNode.replaceNodeData(currentNode.rightChild.key,
+                                    currentNode.rightChild.payload,
+                                    currentNode.rightChild.leftChild,
+                                    currentNode.rightChild.rightChild)
+                
+
 
 
 

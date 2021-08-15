a63eec9004eb2a84c04dc19030bc091b8ac5a4bc
Deborah Ong <deborah.oyt@gmail.com>
2021-08-14 18:05:05 -0700

    add readme

--- trees.py
+++ trees.py
@@ -172,7 +172,24 @@ def minChild(self, i):
             return i * 2
         else:
             return i * 2 + 1
-            
+
+def delMin(self):
+    retval = self.heapList[1]
+    self.heapList[1] = self.heapList[self.currentSize]
+    self.currentSize = self.currentSize - 1
+    self.heapList.pop()
+    self.percDown(1)
+    return retval
+
+#code to build the heap
+def buildHeap(self, alist):
+    i = len(alist) // 2
+    self.currentSize = len(alist)
+    self.heapList = [0] + alist[:]
+    while (i > 0):
+        self.percDown(i)
+        i = i - 1
+
 
 
 

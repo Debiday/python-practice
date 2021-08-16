75263c7ef42a3180eab8a0cc1511b3742a81e9dd
Deborah Ong <deborah.oyt@gmail.com>
2021-08-15 23:35:36 -0700

    add bst problem

--- trees.py
+++ trees.py
@@ -445,31 +445,43 @@ class BinarySearchTree:
 # yellow
 
 # _________________________________________________________
-# check if BST
+# Tree level order print
 # _________________________________________________________
-# if BST then inorder traversal = sorted values
+import collections
 
-tree_vals = []
+class Node:
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
 
-def inorder(tree):
-    if tree != None:
-        inorder(tree.getLeftChild())
-        tree_vals.append(tree.getRootVal())
-        inorder(tree.getRightChild())
+    def levelOrderPrint(tree):
+        
+        if not tree:
+            return
 
-def sort_check(tree_vals):
-    return tree_vals == sorted(tree_vals)
+        nodes = collections.deque([tree])
+        # set up nodes at a deque
+        
+        currentCount = 1
+        nextCount = 0
 
-# inorder(tree)
-# sort_check(tree_vals)
+        while len(nodes) != 0:
 
+            currentNode = nodes.popleft()
+            currentCount -= 1
 
+            print(currentNode.val)
 
+            if currentNode.left:
+                nodes.append(currentNode.left)
+                nextCount += 1
 
+            if currentNode.right:
+                #an if not none check
+                nodes.append(currentNode.right)
+                nextCount += 1
 
-
-
-
-
-
-
+            if currentCount == 0:
+                print('\n')
+                currentCount, nextCount = nextCount, currentCount

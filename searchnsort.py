b043089bdb07d430fa95954852dced7e92c6ca9b
Deborah Ong <deborah.oyt@gmail.com>
2021-08-24 10:25:54 -0700

    add sorting notes

--- searchnsort.py
+++ searchnsort.py
@@ -24,11 +24,51 @@ def ordered_seq_search(arr, ele):
         
     return found
 # _________________________________________________________
-# 
+# Binary Search
 # _________________________________________________________
+def binary_search(arr, ele):
+    """
+    sorted array
+    """
+
+    first = 0
+    last = len(arr)-1
+
+    found = False
+
+    while first <= last and not found:
+
+        mid = (first+last)/2 
+
+        if arr[mid] == ele:
+            found = True
+        else:
+            if ele < arr[mid]:
+                last = mid - 1
+            else: 
+                first = mid + 1
+    return found
 # _________________________________________________________
-# 
+# recursive bs
 # _________________________________________________________
+def rec_bin_search(arr, ele):
+
+    if len(arr) == 0:
+        return False
+
+    else:
+
+        mid = int(len(arr)/2)
+
+        if arr[mid] == ele:
+            return True
+
+        else: 
+            if ele < arr[mid]:
+                return rec_bin_search(arr[:mid], ele)
+
+            else:
+                return rec_bin_search(arr[mid+1:], ele)        
 # _________________________________________________________
 # 
 # _________________________________________________________

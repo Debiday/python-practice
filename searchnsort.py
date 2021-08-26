0797042ab777cb6c5fc969ece41eda428838542b
Deborah Ong <deborah.oyt@gmail.com>
2021-08-25 20:54:17 -0700

    add insertion problem

--- searchnsort.py
+++ searchnsort.py
@@ -70,17 +70,127 @@ def rec_bin_search(arr, ele):
             else:
                 return rec_bin_search(arr[mid+1:], ele)        
 # _________________________________________________________
-# 
+# hashing
 # _________________________________________________________
+#python has dict 
+class HashTable(object):
+
+    def __init__(self, size):
+
+        self.size = size
+        self.slots = [None] * self.size
+        # none is a list with no items, e.g list of 'None's
+        self.data = [None] * self.size
+
+    def put(self, key, data):
+
+        hashvalue = self.hashfunction(key, len(self.slots))
+
+        if self.slots[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
+
+        else: 
+
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data 
+
+            else:
+
+                nextslot = self.rehash(hashvalue, len(self.slots))
+
+                while self.slots[nextslot] != None and self.slots[nextslot] != key:
+                    nextslot = self.rehash(nextslot, len(self.slots))
+
+                if self.slots[nextslot] == None:
+                    self.slots[nextslot] = key
+                    self.data[nextslot] = data
+                
+                else:
+                    self.data[nextslot] = data
+
+    def hashfunction(self, key, size):
+        return key%size
+
+    def rehash(self, oldhash, size):
+        return(oldhash + 1)%size
+
+    def get(self, key):
+
+        startslot = self.hashfunction(key, len(self.slots))
+        data = None
+        stop = False
+        found = False
+        position = startslot
+
+        while self.slots[position] != None and not found and not stop:
+
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+
+            else:
+                position =  self.rehash(position, len(self.slots))
+                if position == startslot:
+
+                    stop = True
+        return data
+
+    def __getitem__(self, key):
+        # special methods for python indexing
+        return self.get(key)
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
 # _________________________________________________________
-# 
+# bubble sort
 # _________________________________________________________
+def bubble_sort(arr):
+    for n in range(len(arr)-1, 0, -1):
+        for k in range(n):
+
+            if arr[k] > arr[k+1]:
+                temp = arr[k]
+                arr[k] = arr[k+1]
+                arr[k+1] = temp
+    return arr
 # _________________________________________________________
-# 
+# selection sort
 # _________________________________________________________
+def selection_sort(arr):
+
+    for fillslot in range(len(arr)-1, 0, -1):
+
+        positionOfMax = 0
+
+        for location in range(1, fillslot+1):
+
+            if arr[location] > arr[positionOfMax]:
+                positionOfMax = location
+
+        temp = arr[fillslot]
+        arr[fillslot] = arr[positionOfMax]
+        arr[positionOfMax] = temp
+        #switch
 # _________________________________________________________
-# 
+# insertion sort
 # _________________________________________________________
+#less efficient on large lists than more advanced algos such as
+#quicksort, heapsort or mergesort
+def insertion_sort(arr):
+
+    for i in range(1, len(arr)):
+
+        currentvalue = arr[i]
+        position = i 
+
+        #sorted sublist
+        while position > 0 and arr[position-1] > currentvalue:
+
+            arr[position] = arr[position-1]
+            position = position - 1
+
+        arr[position] = currentvalue
 # _________________________________________________________
 # 
 # _________________________________________________________

90d619a993d001ef0d50b7009f2d228bde46343d
Deborah Ong <deborah.oyt@gmail.com>
2021-08-31 16:30:18 -0700

    add bfs implementation

--- graphs.py
+++ graphs.py
@@ -106,4 +106,85 @@ def buildGraph(wordFile):
                     g.addEdge(word1, word2)
     
     return g
-    
+
+# _________________________________________________________
+# breadth first search: finding the shortest solution to word ladder
+# _________________________________________________________
+#queue
+# _________________________________________________________
+# implementation of graph, recap
+# _________________________________________________________
+#stack
+from enum import Enum
+from collections import OrderedDict
+
+class State(Enum):
+    unvisited =  1
+    visited = 2
+    visiting = 3
+
+class Node:
+
+    def __init__(self, num):
+
+        self.num = num
+        self.visit_state = State.unvisited
+        self.adjacent = OrderedDict() #key = node, value = weight
+
+    def __str__(self):
+        return str(self.num)
+
+class Graph:
+
+    def __init__(self):
+        self.nodes = OrderedDict()
+
+    def add_node(self, num):
+
+        node = Node(num)
+        self.nodes[num] = node
+        return node
+
+    def add_edge(self, source, dest, weight=0):
+
+        if source not in self.nodes:
+            self.add_node(source)
+        if dest not in self.nodes:
+            self.add_node(dest)
+
+        self.nodes[source].adjacent[self.nodes[dest]] = weight
+# _________________________________________________________
+# implementation of dfs
+# _________________________________________________________
+def dfs(graph, start):
+    visited = set()
+    stack = [start]
+
+    while stack:
+        vertex = stack.pop()
+
+        if vertex not in visited:
+            visited.add(vertex)
+
+            stack.extend(graph[vertex] - visited)
+
+    return visited 
+# _________________________________________________________
+# implementation of bfs
+# _________________________________________________________
+#same results as dfs but provides shortet path first
+#uses queue
+def bfs(graph, start):
+    visited = set()
+    queue = [start]
+
+    while queue: 
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+
+    return visited
+
+
+#check: return all possible paths

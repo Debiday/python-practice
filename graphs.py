8281ee21659c0175212a982446099bb2965d4bab
Deborah Ong <deborah.oyt@gmail.com>
2021-08-31 00:24:54 -0700

    add graph

--- graphs.py
+++ graphs.py
@@ -1,3 +1,67 @@
 # _________________________________________________________
 # adjacency matrix
 # _________________________________________________________
+#not an efficient way to store sparse data
+#good when number of edges is large |V²|
+#connectivity: every vertex is connected to another
+# _________________________________________________________
+# adjacency list
+# _________________________________________________________
+#more space efficient eay to imp a sparsely connected graph (compact)
+class Vertex:
+
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+
+    def addNeighbor(self, nbr, weight=0):
+        self.connectedTo[nbr] = weight
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self, nbr):
+        return self.connectedTo[nbr]
+
+    def __str__(self):
+
+        return str(self.id) + ' connected to: ' + str([x.id for x in self.connectedTo])
+
+class Graph: 
+
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self, key):
+        self.numVertices += 1
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
+
+    def getVertex(self, n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+    
+    def addEdge(self, f, t, cost=0):
+        if f not in self.vertList:
+            nv = self.addVertex(f)
+        if t not in self.vertList:
+            nv = self.addVertex(t)
+        
+        self.vertList[f].addNeighbor(self.vertList[t], cost)
+
+    def getVertices(self):
+        return self.vertList.keys()
+
+    def __iter__(self):
+        return iter(self.vertList.values())
+        #grab all values and make it into an iterable object
+
+    def __contains__(self, n):
+        return n in self.vertList
